4085b94244bf466414a6598f49d49cab28b1ed54
Gavin <gavc8943@gmail.com>
2024-05-28 14:56:21 +0100

    Basic JavaScript Projects completed
    My basic JavaScript projects including:
    declaring variables, concatenating stings,
    commenting code, writing expressions etc

---
+++ Basic JavaScript Pojects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,30 @@
+var A = "This is a string";
+document.write(A);
+document.write("<br>");
+var myString = "My new String"; // creating a string
+window.alert(myString); // Window alert method
+
+document.write('Forrest Gump said,"Life is like a box of chocolates"');
+document.write("<br>");
+document.write("My name is " + "Gavin Cain " + "and I like football."); // Concatenating strings
+
+document.write("<br>"); // creating a line break
+var myConcatenatedString = "This is " + "my concatenated string";
+document.write(myConcatenatedString);
+document.write("<br>");
+var Band = "Led Zeppelin",
+  JohnB = "Drummer",
+  Robert = "Singer",
+  Jimmy = "Guitarist",
+  JohnP = "Bass";
+document.write(Robert);
+document.write("<br>");
+document.write(5 + 5); // an expression with a statement
+document.write("<br>");
+var sent1 = "This is the beginning of the string"; // creating variables
+var sent2 = "and this is the end of the string";
+var sent3 =
+  "This is the beginning of the string " + "and this is the end of the string";
+document.write(sent3);
+document.write("<br>");
+document.write(5 * 5); // another expression
